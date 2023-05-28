778c6930c2527a44d123960bc5779312987e9466
Berat Sönmez <bert.snmz@gmail.com>
2023-05-28 05:47:19 +0300

    Level Desing Complete
    I change level desing and use all tile sets. I resolve main menu bugs. I redesing credits screen.

--- RageQuit/Assets/Scripts/GameInUI/ButtonUrl.cs
+++ RageQuit/Assets/Scripts/GameInUI/ButtonUrl.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class ButtonUrl : MonoBehaviour
 {
-   public void OpenURL()
+   public void BilalGitHubOpen()
     {
         Application.OpenURL("https://github.com/bilalaltinn");
+    }
+
+    public void BilalInstaOpen()
+    {
+        Application.OpenURL("https://www.instagram.com/bilalaltn07/");
+    }
+
+    public void BeratGitHubOpen()
+    {
+        Application.OpenURL("https://github.com/Beratsonmez");
+    }
+
+    public void BeratInstaOpen()
+    {
+        Application.OpenURL("https://www.instagram.com/berat.lik/");
     }
 }
